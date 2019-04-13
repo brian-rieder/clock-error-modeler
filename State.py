5a9a45c744b993bb34080b8858066ac72b23f56d
Brian Rieder <brian.rieder777@gmail.com>
2019-04-13 14:55:19 -0400

    Added printing functions and fixed the tuple operations for transitions.

--- State.py
+++ State.py
@@ -15,23 +15,32 @@ class State:
             print("Attempted to add non-State-type transition state. Exiting...")
             exit(1)
         if not callable(transition[1]):
-            print("Attempted to add non-function action to transition. Exiting...")
+            print("Attempted to add non-function condition to transition. Exiting...")
             exit(1)
         self.transitions += transition
 
-    def addTransition(self, state, action):
+    def addTransition(self, state, condition):
         if type(state) != State:
             print("Attempted to add non-State-type transition state. Exiting...")
             exit(1)
-        if not callable(action):
-            print("Attempted to add non-function action to transition. Exiting...")
+        if not callable(condition):
+            print("Attempted to add non-function condition to transition. Exiting...")
             exit(1)
-        self.transitions += (state, action)
+        self.transitions.append(tuple([state, condition]))
 
     def __init__(self, name):
         self.name = name
         self.transitions = []
         self.actions = []
+
+    def __str__(self):
+        return "State: " + self.name
+
+
+def print_process(state, depth=0):
+    print('\t' * depth + str(state))
+    for transition in state.transitions:
+        print_process(transition[0], depth+1)
 
 
 if __name__ == "__main__":
