4d13adeb963ba636e23d66b62ff4c26e989bdaa1
Brian Rieder <brian.rieder777@gmail.com>
2019-04-13 15:53:54 -0400

    Added support for actions and cleaner, non-infinite printing

--- State.py
+++ State.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python3
 
+import inspect
+
 class State:
     def addAction(self, action):
         if not callable(action):
             print("Attempted to add non-function action. Exiting...")
             exit(1)
-        self.actions += action
+        self.actions.append(action)
 
     def addTransition(self, transition):
-        if type(transition) != tuple:
-            print("Attempted to add non-tuple transition. Exiting...")
+        if type(transition) != dict:
+            print("Attempted to add non-dictionary transition. Exiting...")
             exit(1)
-        if type(transition[0]) != State:
+        if type(transition["state"]) != State:
             print("Attempted to add non-State-type transition state. Exiting...")
             exit(1)
-        if not callable(transition[1]):
+        if not callable(transition["condition"]):
             print("Attempted to add non-function condition to transition. Exiting...")
             exit(1)
         self.transitions += transition
@@ -26,7 +28,7 @@ class State:
         if not callable(condition):
             print("Attempted to add non-function condition to transition. Exiting...")
             exit(1)
-        self.transitions.append(tuple([state, condition]))
+        self.transitions.append({"state":state, "condition":condition})
 
     def __init__(self, name):
         self.name = name
@@ -37,10 +39,19 @@ class State:
         return "State: " + self.name
 
 
+def print_state(state):
+    print(state)
+    for transition in state.transitions:
+        func = transition["condition"]
+        funcString = str(inspect.getsourcelines(func)[0])
+        funcString = funcString.strip("['\\n']").split(" = ")[1]
+        print("\t", transition["state"], ", " + funcString)
+
+
 def print_process(state, depth=0):
     print('\t' * depth + str(state))
     for transition in state.transitions:
-        print_process(transition[0], depth+1)
+        print_process(transition["state"], depth+1)
 
 
 if __name__ == "__main__":
